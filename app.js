932caaf3be2a20715b165d4b4d0862913897e2d3
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-06-12 01:07:54 +0200

    complete

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
 var quizRouter = require('./routes/quiz');
 var adminRouter = require('./routes/admin');
+var apiRouter=require('./routes/api')
 // var usersRouter = require('./routes/users');
 
 var app = express();
@@ -54,6 +55,7 @@ app.use('/', indexRouter);
 app.use('/news', newsRouter);
 app.use('/quiz', quizRouter);
 app.use('/admin', adminRouter);
+app.use('/api', apiRouter)
 
 // app.use('/users', usersRouter);
 
