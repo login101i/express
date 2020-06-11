5834d63b0da00ab9d10e62e1e537576173e387b5
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-06-12 00:14:05 +0200

    quiz form

--- models/news.js
+++ models/news.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const Schema= mongoose.Schema
+const Schema = mongoose.Schema
 
-var newsSchema = new Schema({ 
-    title: {type:String, required:[true, "Pole tytuł jest wymagane"]},
-    description:{ type:String, required:true},
-    created:{type:Date, default:Date.now}
+var newsSchema = new Schema({
+    title: { type: String, required: [true, "Pole tytuł jest wymagane"] },
+    description: { type: String, required: true },
+    created: { type: Date, default: Date.now }
 
 });
 
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('quiz', { title: 'Witaj w quiz ie' });
+    const show = req.session.vote
+    // new Quiz({ title: 'Pytanie 1', vote: 0 }).save()
+
+    Quiz.find({}, (err, data) => {
+        let sum=0;
+        data.forEach((item)=>{
+            sum+=item.vote;
+        })
+        res.render('quiz', { title: 'Witaj w quiz ie', data , show, sum});
+    })
+
 });
+
+router.post('/', (req, res) => {
+    const id = req.body.quiz
+
+    Quiz.findOne({_id:id}, (err, data) => {
+       console.log(data)
+       data.vote=data.vote+1
+       data.save();
+       res.redirect('/quiz')
+    })
+   
+})
 
 module.exports = router;
